48009d6e4f17e6020cf7dd6bfdf3984a0e07fdf2
Cryfari <kevinsudrajat27@gmail.com>
2022-11-25 10:13:01 +0700

    update detail route

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::controller(GradeController::class)->group(function(){
     Route::get('/nilai', 'nilai')->name('nilai');
     Route::get('/nilai/add', 'add')->name('add');
     Route::post('/nilai/add/process', 'addprocess')->name('addprocess');
-    Route::get('/nilai/detail{id}', 'detail')->name('detail');
+    Route::get('/nilai/detail/{id}', 'detail')->name('detail');
     Route::get('/nilai/update/{id}', 'update')->name('update');
     Route::put('/nilai/update/{id}/{image}/process', 'updateprocess')->name('updateprocess');
     Route::get('/nilai/delete/{id}', 'delete')->name('delete');
